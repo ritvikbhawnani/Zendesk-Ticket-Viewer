774315589178244ca45723577d93542a6609f57a
Ritvik Bhawnani <ritvikbhawnani@gmail.com>
2021-08-04 01:44:50 +0200

    Create view structure

---
+++ views/TicketView.py
@@ -0,0 +1,53 @@
+class TicketView:
+    """
+    The TicketView presents the data to the user
+
+    """
+
+    def display_hello_message(self):
+        """
+        Displays a welcome message at the start
+
+        """
+
+        print("hello_message")
+
+    def display_menu(self):
+        """
+        Displays the menu of available options
+
+        """
+
+        print("menu")
+
+    def display_goodbye_message(self):
+        """
+        Displays a thank you message
+
+        """
+
+        print("goodbye")
+
+    def display_all_tickets(self):
+        """
+        Displays all the tickets
+
+        """
+
+        print("all tickets")
+
+    def display_ticket_detail(self):
+        """
+        Displays the details of a single ticket
+
+        """
+
+        print("ticket detail")
+
+    def display_error_message(self):
+        """
+        Displays error messages
+
+        """
+
+        print("error")
